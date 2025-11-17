6805e7f33a147baa7c3688feaa00966d3ea24040
Crazy-Bull <yuhehuang@sjtu.edu.cn>
2025-11-18 00:03:05 +0800

    csv report and some new results

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # 4 spawn rate (default: 0.1)
 P = 0.1
 # name of temporary folder (default: 'tmp') (make sure that two processes do not use the same folder)
-TEMP_FOLDER = 'tmp'
+TEMP_FOLDER = 'tmp4'
 # whether to use cProfile for timing (default: False)
 USE_CPROFILE = False
 # list of polynomino indices to process (default: list(range(0, 369)))
-workflow = list(range(0, 369))
+workflow = list(range(52, 53))
